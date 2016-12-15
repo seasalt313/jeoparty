0dd428c6fde4723da5e280bda4605a8cb668cd1f
Mariam Qasim <mariam.a.qasim@gmail.com>
2016-12-15 10:45:25 -0500

    in-class review added

--- jeopartyinclass/app.js
+++ jeopartyinclass/app.js
@@ -1,3 +1,12 @@
+//-- make variable to share across global scopes:
+let current = {
+    answer: null,
+    points: 0
+}; // the current question
+
+let score = 0; //global variable that goes up when question is right
+
+
 // Your challenge:
 //
 // Retrieve a question when the page loads and display it on the screen. Each question should display the category, point value, and the question text. You should also have a text box that people can type their answer into, and a button they can click to submit their answer.
@@ -35,6 +44,10 @@ function init() {
 
       show(response[0]);
 
+      //save important information about this question
+      current.answer = response[0].answer;
+      current.points = response[0].value;
+
     })
 
 
@@ -50,14 +63,29 @@ function show(stuff){
   let ans = document.querySelector("h6");
 
   category.textContent = "The category is: " + stuff.category.title;
-
   question.textContent = "The question is : " + stuff.question;
-
   points.textContent = "The value of this answer is: " + stuff.value;
-
   ans.textContent = "The answer is: " + stuff.answer;
-
-
 }
+
+
+function checkAnswer(){
+  // -check if the text they typed in (Guess) is the same as the answer to the question.
+  let guess = document.querySelector('#guess').value;
+  console.log(guess);
+  let answer = 'something';
+
+  if (guess === current.answer){
+    //give points
+    score = score + current.points;
+  }
+  go();
+}
+
+
+
+
+
+
 
 window.addEventListener('load', go);
